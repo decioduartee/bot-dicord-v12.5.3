0bc4f45e58b62296fb6714fd512b1271a793aee2
decionunes <decionnesd44@gmail.com>
2021-04-06 15:36:50 -0300

    primeiro commit

--- comandos/ping.js
+++ comandos/ping.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ping',
     aliases: ['ping'],
     async execute(client, message, args) {
-        message.channel.send('funciona');
+        message.channel.send('Pong!');
     }
 }
